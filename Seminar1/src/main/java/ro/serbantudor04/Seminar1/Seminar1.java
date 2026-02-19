fcb809b84943f25284f2b1ebd1f9f7601d643562
Serban Tudor Gabriel <tudor.gabriel.serban@gmail.com>
2026-02-19 16:16:14 +0200

    Initial commit

---
+++ Seminar1/src/main/java/ro/serbantudor04/Seminar1/Seminar1.java
@@ -0,0 +1,9 @@
+package ro.serbantudor04.Seminar1;
+
+public class Seminar1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "ActivitateCTS2026_Serban_Tudor_Gabriel"
+include("Seminar1")
