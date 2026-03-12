2dd5cc8d845121d323d44e7ee5a823a6a7cb89dd
Serban Tudor Gabriel <tudor.gabriel.serban@gmail.com>
2026-03-12 16:01:30 +0200

    Add Seminar4 module with initial setup and `Seminar4` class

---
+++ Seminar4/src/main/java/ro/serbantudor04/seminar4/Seminar4.java
@@ -0,0 +1,5 @@
+package ro.serbantudor04.seminar4;
+
+public class Seminar4 {
+    public static void main(String[] args) {}
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 rootProject.name = "ActivitateCTS2026_Serban_Tudor_Gabriel"
 include("Seminar1")
 include("Seminar2")
+include("Seminar4")
